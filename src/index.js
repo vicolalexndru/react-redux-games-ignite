e0d098175275301b95f3e2b846369761fb5b6b5c
alex <contact@alex-vicol.com>
2021-06-02 19:34:10 +0100

    Redux set up

--- src/index.js
+++ src/index.js
@@ -4,9 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Redux setup
+import {createStore, applyMiddleware, compose} from 'redux';
+import rootReducer from './reducers';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+
+const composeEnchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnchancer(applyMiddleware(thunk))
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+   <Provider store = {store}> 
+       <App />
+   </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,17 @@
+const initState = {
+    popular: [],
+    newGames: [],
+    upcoming: []
+}
+
+const gameReducer = (state = initState, action) => {
+   switch (action.type) {
+       case 'FETCH_GAMES':
+         return{ ...state }
+   
+       default:
+           return {...state}
+   }
+}
+
+export default gameReducer;
